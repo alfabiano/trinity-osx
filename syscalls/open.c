e54f89bf712cefb58be719ba3dbd4c7c85d87aaa
eQuiNoX__ <zubin.mithra@gmail.com>
2012-04-19 10:54:55 +0530

    [modify] open.c

--- syscalls/open.c
+++ syscalls/open.c
@@ -21,13 +21,10 @@ struct syscall syscall_open = {
 	.arg2type = ARG_LIST,
 	.arg2list = {
 		.num = 19,
-		.values = { O_RDONLY, O_WRONLY, O_RDWR,
-				O_CREAT, O_EXCL, O_NOCTTY,
-				O_TRUNC, O_APPEND, O_NONBLOCK,
-				O_SYNC, O_ASYNC,
-				O_DIRECTORY, O_NOFOLLOW, O_CLOEXEC,
-				O_DIRECT, O_NOATIME, O_PATH,
-				O_DSYNC, O_LARGEFILE },
+		.values = { O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_EXCL, O_NOCTTY,
+			    O_TRUNC, O_APPEND, O_NONBLOCK, O_SYNC, O_ASYNC,
+			    O_DIRECTORY, O_NOFOLLOW, O_PATH, O_DSYNC,
+			    O_SHLOCK, O_EXLOCK, O_SYMLINK, O_EVTONLY },
 	},
 	.arg3name = "mode",
 };
